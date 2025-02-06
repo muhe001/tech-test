5797f1a67a95aab637d2e45630fc93c6f9961a14
martin uhe <martin.uhe@gmail.com>
2025-02-07 11:07:01 +1300

    outputtred hex string of packet ids as per instructions

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import processMpegStream from './processMpegStream';
 const stream = process.stdin;
 
 processMpegStream(stream, 188).then(results => {
-  console.log(`results: ${results}`);
+  results.forEach(id => console.log(`0x${id.toString(16)}`));
   process.exit(0)
 }).catch(err => {
   console.error(err);
